6bc8808e8909640a374817505ffd1baadbc00a03
JYPUY <jypuy1234@naver.com>
2025-09-17 23:06:41 +0900

    Update and rename 9_16 to 9_16.py

--- 9_16.py
+++ 9_16.py
@@ -7,3 +7,10 @@ for i in range(1,n+1):
         add+=j
     result += add
 print(result)
+9/17
+n = int(input())
+
+for i in range(1,7):
+    for j in range(1,7):
+        if i+j == n:
+            print(i,j)
